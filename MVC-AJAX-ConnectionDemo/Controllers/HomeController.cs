ad58d9e52c36d4036c5c177bc8a84db89d139281
thenabeelahmad <nabeelahmad4644@gmail.com>
2021-06-13 11:54:02 +0530

    AJAX MVC

---
+++ MVC-AJAX-ConnectionDemo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient; 
+
+
+namespace MVC_AJAX_ConnectionDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+
+        DBManager dm = new DBManager();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult InsertRecord(string n, string c)
+        {
+            string q = "insert into tbl_testdb values('" + n + "','" + c + "')";
+            bool res = dm.InsertUpdateDelete(q);
+            string confirm;
+            if (res == true)
+            {
+                confirm = "success";
+            }
+            else
+            {
+                confirm = "failure";
+            }
+
+            return Json(confirm, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
